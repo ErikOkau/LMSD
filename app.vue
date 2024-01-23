5b4ff27f5beef2fc29577486a9cb06f1e093cade
ErikOkau <erikmsorheim@yahoo.no>
2024-01-23 15:49:44 +0100

    Navbar design almost complete

--- app.vue
+++ app.vue
@@ -12,6 +12,14 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
+:root {
+    --darkest:#162946;
+    --dark:#284368;
+    --light:#fff;
+    --black: #000;
+}
+
+
 * {
     margin: 0;
     padding: 0;  
@@ -19,5 +27,6 @@
 
 body {
     font-family: 'Inter', sans-serif;
+    background-color: var(--darkest);
 }
 </style>
--- components/Nav.vue
+++ components/Nav.vue
@@ -1,21 +1,152 @@
 <script setup lang="ts">
 
+const modeShift = ref(false)
+
+const modeShiftHandler = () => {
+    modeShift.value = !modeShift.value
+
+    if (modeShift.value) {
+        document.documentElement.style.setProperty('--darkest', '#fff')
+        document.documentElement.style.setProperty('--dark', '#000')
+        document.documentElement.style.setProperty('--light', '#000')
+    } else {
+        document.documentElement.style.setProperty('--darkest', '#162946')
+        document.documentElement.style.setProperty('--dark', '#284368')
+        document.documentElement.style.setProperty('--light', '#fff')
+    }
+}
+
 </script>
 
 <template>
     <nav>
         <div class="left_navbar">
-            <img src="assets/logo.png" alt="logo">
+            <h1 class="logo">Gaming Codex</h1>
+            <div class="navigation_links">
+                <NuxtLink to="/">Profiles</NuxtLink>
+                <NuxtLink to="/">Leaderboards</NuxtLink>
+            </div>
         </div>
         <div class="right_navbar">
-            <NuxtLink to="/">Log in</NuxtLink>
-            <img src="assets/user.png" alt="dark_lightMode">
-            
-            <Dropdown_lang />
+            <NuxtLink class="Login" to="/">Log in</NuxtLink>
+            <img src="/img/light_mode.svg" alt="light_mode" @click="modeShiftHandler" v-if="!modeShift">
+            <img src="/img/dark_mode.svg" alt="dark_mode" @click="modeShiftHandler" v-else>
+
+            <div class="languageSelect_dropdown">
+                <select name="languageSelect" id="languageSelect">
+                    <option value="english">English</option>
+                    <option value="norwegian">Norwegian</option>
+                </select>
+            </div>
         </div>
     </nav>
 </template>
 
 <style scoped lang="scss">
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5rem;
+    height: 10vh;
 
+    .left_navbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .navigation_links {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 2rem;
+            margin-left: 2rem;
+
+
+            a {
+                transition: all 0.3s ease-in-out;
+                color: var(--light);
+                text-decoration: none;
+                padding: 0.5rem;
+                transition: all 0.3s ease-in-out;
+                border-radius: 10;
+                text-decoration: none;
+
+                &:hover {
+                    cursor: pointer;
+                    color: var(--light);    
+                    background-color: var(--dark);
+                }
+            }
+        }
+
+        .logo {
+            transition: all 0.3s ease-in-out;
+            color: var(--light);
+
+            &:hover {
+                cursor: pointer;
+                box-shadow: 0px 1px 0px 0px;
+                color: var(--light);
+            }
+        }
+    }
+
+    .right_navbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.1rem;
+
+        .Login {
+            color: var(--light);
+            padding: 0.5rem;
+            transition: all 0.3s ease-in-out;
+            border-radius: 10;
+            text-decoration: none;
+
+            &:hover {
+                cursor: pointer;
+                background-color: var(--dark);
+                color: #fff;
+            }
+        }
+
+        img {
+            width: 30px;
+            margin: 0 1rem;
+            transition: all 0.3s ease-in-out;
+            padding: 0.3rem;
+            border-radius: 10;
+
+            &:hover {
+                cursor: pointer;
+                box-shadow: 1px 1px 2px 2px var(--dark);
+                background-color: var(--dark);
+            }
+        }
+
+        select {
+            padding: 0.5rem;
+            border-radius: 10;
+            background-color: var(--dark);
+            color: white;
+            border: none;
+            transition: all 0.3s ease-in-out;
+            border-radius: none;
+
+            option {
+                width: 100%;
+                border-radius: none;
+                background-color: var(--dark);
+                color: white;
+            }
+
+            &:hover {
+                cursor: pointer;
+                background-color: var(--dark);
+            }
+        }
+    }
+}
 </style>
